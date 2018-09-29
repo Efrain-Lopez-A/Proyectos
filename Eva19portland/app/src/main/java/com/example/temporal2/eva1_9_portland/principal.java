6181776372250b65c7a29ca0ba43b2c45adaad0c
EfrainLopez <efrain.loapz@gmail.com>
2018-09-28 18:40:21 -0600

    Practica 9
    Eva1-9-Portland

---
+++ Eva19portland/app/src/main/java/com/example/temporal2/eva1_9_portland/principal.java
@@ -0,0 +1,13 @@
+package com.example.temporal2.eva1_9_portland;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
